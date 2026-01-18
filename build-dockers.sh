19093598a0ed16a51abc380d933ac03a355fa00c
Nikolai Giman <gimanhead@gmail.com>
2026-01-18 22:13:59 +0100

    chore: deps

--- build-dockers.sh
+++ build-dockers.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
-# This script builds the docker images for the taskview-ce-monorepo
+# This script builds the docker images for TaskView Community Edition
 # - taskview-ce-api-server
 # - taskview-ce-db-migration
 # - taskview-ce-webapp
 
-VERSION=$(node -p "require('./package.json').version")
+set -e
 
-echo $VERSION
+VERSION=$1
 
+# If version not provided, read from package.json
+if [ -z "$VERSION" ]; then
+    VERSION=$(node -p "require('./package.json').version")
+fi
+
+echo "Building TaskView CE version: $VERSION"
+
+# Build CE API
+echo "========================================="
+echo "Building CE API Server..."
+echo "========================================="
 cd api
 bash build-docker-api.sh $VERSION
 cd ..
 
-
+# Build CE Web
+echo "========================================="
+echo "Building CE Web App..."
+echo "========================================="
 cd web
 bash build-docker-web.sh $VERSION
 cd ..
+
+echo "========================================="
+echo "Build complete!"
+echo "Images built:"
+echo "  - gimanhead/taskview-ce-api-server:$VERSION"
+echo "  - gimanhead/taskview-ce-webapp:$VERSION"
+echo "========================================="
